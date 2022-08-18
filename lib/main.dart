871b4e59b633086e2feb73d449a5313165bf828a
afexirodrigue <afexirodrigue@gmail.com>
2022-08-18 14:40:50 +0000

    quotes flutter netninja project quote.text quote.author error

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  final List<Quote> quotes = <Quote>[
+    Quote(
+        author: 'Houphouet-Boigny',
+        text: "La paix n'est pas un vain mot mais un comportement"),
+    Quote(author: "L'Ecclesiaste", text: "L'humilité précède la gloire"),
+    Quote(
+        author: 'Lao Tseu',
+        text: "Un voyage de mille lieues commence toujours par un premier pas"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Citations inspirantes'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+      ),
+    );
+  }
+}
